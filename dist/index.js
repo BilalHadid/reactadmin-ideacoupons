21d6f571ce31cd7678c2aae33780f2840731d294
bilalhadid <hadid0648@gmail.com>
2020-11-29 20:52:06 +0500

    Inittial

--- dist/index.js
+++ dist/index.js
@@ -123,34 +123,39 @@ var convertFileToBase64 = function convertFileToBase64(file) {
   return new Promise(function (resolve, reject) {
     var reader = new FileReader();
     reader.onload = function () {
-      return resolve({ result: reader.result, type: file.type, title: file.title, size: file.rawFile.size });
+      return resolve({
+        result: reader.result,
+        type: file.type,
+        title: file.title,
+        size: file.rawFile.size
+      });
     };
     reader.onerror = reject;
     reader.readAsDataURL(file.rawFile);
   });
 };
 var index = (function (firebase) {
-  var settings = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : { context: '', imagekey: 'images', filekey: 'files' };
+  var settings = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : { context: "", imagekey: "images", filekey: "files" };
 
   var database = firebase.default.database();
   return {
     create: function create(source, params) {
-      var resource = [settings.context, source].join('/');
+      var resource = [settings.context, source].join("/");
       var uid = params.data.id || database.ref().child(resource).push().key;
 
       var create = !params.data[settings.imagekey] && !params.data[settings.filekey] ? new Promise(function (resolve, reject) {
-        var ref = database.ref([resource, uid].join('/'));
+        var ref = database.ref([resource, uid].join("/"));
         ref.set(_extends({}, params.data, { id: uid }));
         resolve();
       }) : Promise.all((!params.data[settings.imagekey] ? [] : params.data[settings.imagekey] instanceof Array ? params.data[settings.imagekey] : [params.data[settings.imagekey]]).map(function (img) {
-        return _extends({}, img, { type: 'image' });
+        return _extends({}, img, { type: "image" });
       }).map(convertFileToBase64).concat((!params.data[settings.filekey] ? [] : params.data[settings.filekey] instanceof Array ? params.data[settings.filekey] : [params.data[settings.filekey]]).map(function (file) {
-        return _extends({}, file, { type: 'file' });
+        return _extends({}, file, { type: "file" });
       }).map(convertFileToBase64))).then(function (base64ed) {
         return base64ed.map(function (item64, key) {
           return {
             src: item64.result,
-            id: params.data.id + '_' + item64.type + '_' + key,
+            id: params.data.id + "_" + item64.type + "_" + key,
             title: item64.title,
             type: item64.type,
             size: item64.size
@@ -159,11 +164,11 @@ var index = (function (firebase) {
       }).then(function (transformedNewItems) {
         var _babelHelpers$extends;
 
-        database.ref([resource, uid].join('/')).set(_extends({}, params.data, (_babelHelpers$extends = {}, defineProperty(_babelHelpers$extends, settings.imagekey, transformedNewItems.filter(function (i) {
-          return i.type === 'image';
+        database.ref([resource, uid].join("/")).set(_extends({}, params.data, (_babelHelpers$extends = {}, defineProperty(_babelHelpers$extends, settings.imagekey, transformedNewItems.filter(function (i) {
+          return i.type === "image";
         })), defineProperty(_babelHelpers$extends, settings.filekey, transformedNewItems.filter(function (i) {
-          return i.type === 'file';
-        })), defineProperty(_babelHelpers$extends, 'id', uid), _babelHelpers$extends)));
+          return i.type === "file";
+        })), defineProperty(_babelHelpers$extends, "id", uid), _babelHelpers$extends)));
       });
 
       return create.then(function (res) {
@@ -171,24 +176,23 @@ var index = (function (firebase) {
       });
     },
     update: function update(source, params) {
-      var resource = [settings.context, source].join('/');
+      var resource = [settings.context, source].join("/");
       var update = !params.data[settings.imagekey] && !params.data[settings.filekey] ? new Promise(function (resolve, reject) {
-        database.ref([resource, params.id].join('/')).set(params.data);
+        database.ref([resource, params.id].join("/")).set(params.data);
         resolve();
       }) : Promise.all((!params.data[settings.imagekey] ? [] : params.data[settings.imagekey] instanceof Array ? params.data[settings.imagekey] : [params.data[settings.imagekey]]).filter(function (p) {
         return p.rawFile instanceof File;
       }).map(function (img) {
-        return _extends({}, img, { type: 'image' });
+        return _extends({}, img, { type: "image" });
       }).map(convertFileToBase64).concat((!params.data[settings.filekey] ? [] : params.data[settings.filekey] instanceof Array ? params.data[settings.filekey] : [params.data[settings.filekey]]).filter(function (p) {
         return p.rawFile instanceof File;
       }).map(function (file) {
-        return _extends({}, file, { type: 'file' });
+        return _extends({}, file, { type: "file" });
       }).map(convertFileToBase64))).then(function (base64ed) {
         return base64ed.map(function (item64, key) {
           return {
-
             src: item64.result,
-            id: params.data.id + '_' + item64.type + '_' + key,
+            id: params.data.id + "_" + item64.type + "_" + key,
             title: item64.title,
             type: item64.type,
             size: item64.size
@@ -197,12 +201,12 @@ var index = (function (firebase) {
       }).then(function (transformedNewItems) {
         var _babelHelpers$extends2;
 
-        database.ref([resource, params.id].join('/')).set(_extends({}, params.data, (_babelHelpers$extends2 = {}, defineProperty(_babelHelpers$extends2, settings.imagekey, [].concat(toConsumableArray(transformedNewItems.filter(function (i) {
-          return i.type === 'image';
+        database.ref([resource, params.id].join("/")).set(_extends({}, params.data, (_babelHelpers$extends2 = {}, defineProperty(_babelHelpers$extends2, settings.imagekey, [].concat(toConsumableArray(transformedNewItems.filter(function (i) {
+          return i.type === "image";
         })), toConsumableArray((!params.data[settings.imagekey] ? [] : params.data[settings.imagekey] instanceof Array ? params.data[settings.imagekey] : [params.data[settings.imagekey]]).filter(function (p) {
           return !(p.rawFile instanceof File);
         })))), defineProperty(_babelHelpers$extends2, settings.filekey, [].concat(toConsumableArray(transformedNewItems.filter(function (i) {
-          return i.type === 'file';
+          return i.type === "file";
         })), toConsumableArray((!params.data[settings.filekey] ? [] : params.data[settings.filekey] instanceof Array ? params.data[settings.filekey] : [params.data[settings.filekey]]).filter(function (p) {
           return !(p.rawFile instanceof File);
         })))), _babelHelpers$extends2)));
@@ -218,10 +222,10 @@ var index = (function (firebase) {
           while (1) {
             switch (_context.prev = _context.next) {
               case 0:
-                resource = [settings.context, source].join('/');
+                resource = [settings.context, source].join("/");
                 field = params.sort.field;
                 ref = database.ref(resource);
-                return _context.abrupt('return', ref.orderByChild(field).once('value').then(function (snapshot) {
+                return _context.abrupt("return", ref.orderByChild(field).once("value").then(function (snapshot) {
                   var valuesToReturn = snapshot.val() ? Object.values(snapshot.val()) : [];
                   if (params.filter) {
                     var filterset = params.filter;
@@ -234,9 +238,9 @@ var index = (function (firebase) {
                           filterValue = _ref3[1];
 
                       valuesToReturn = valuesToReturn.filter(function (value) {
-                        if (value && value[filterKey] && _typeof(value[filterKey]) !== 'object') {
-                          var propsToFilter = ('' + value[filterKey]).toLowerCase();
-                          if (propsToFilter.includes(('' + filterValue).toLowerCase())) {
+                        if (value && value[filterKey] && _typeof(value[filterKey]) !== "object") {
+                          var propsToFilter = ("" + value[filterKey]).toLowerCase();
+                          if (propsToFilter.includes(("" + filterValue).toLowerCase())) {
                             return true;
                           }
                         }
@@ -249,7 +253,7 @@ var index = (function (firebase) {
                     var values = [];
                     values = valuesToReturn;
                     if (params.sort) {
-                      values.sort(sortBy__default['default']('' + (params.sort.order === 'ASC' ? '-' : '') + params.sort.field));
+                      values.sort(sortBy__default['default']("" + (params.sort.order === "ASC" ? "-" : "") + params.sort.field));
                     }
                     var _params$pagination = params.pagination,
                         page = _params$pagination.page,
@@ -262,12 +266,12 @@ var index = (function (firebase) {
 
                     return { data: data, total: total };
                   } else {
-                    throw new Error('Error processing request');
+                    throw new Error("Error processing request");
                   }
                 }));
 
               case 4:
-              case 'end':
+              case "end":
                 return _context.stop();
             }
           }
@@ -286,19 +290,20 @@ var index = (function (firebase) {
           while (1) {
             switch (_context2.prev = _context2.next) {
               case 0:
-                resource = [settings.context, source].join('/');
+                resource = [settings.context, source].join("/");
                 _context2.next = 3;
-                return database.ref([resource, params.id].join('/'));
+                return database.ref([resource, params.id].join("/"));
 
               case 3:
                 ref = _context2.sent;
-                return _context2.abrupt('return', ref.once('value').then(function (snapshot) {
+                return _context2.abrupt("return", ref.once("value").then(function (snapshot) {
                   return {
-                    data: snapshot.val() };
+                    data: snapshot.val()
+                  };
                 }));
 
               case 5:
-              case 'end':
+              case "end":
                 return _context2.stop();
             }
           }
@@ -311,10 +316,10 @@ var index = (function (firebase) {
     }(),
 
     getMany: function getMany(source, params) {
-      var resource = [settings.context, source].join('/');
+      var resource = [settings.context, source].join("/");
       var getMany = new Promise(function (resolve, reject) {
         var data = params.ids.map(function (id) {
-          return database.ref([resource, id].join('/')).once('value').then(function (snapshot) {
+          return database.ref([resource, id].join("/")).once("value").then(function (snapshot) {
             return snapshot.val();
           });
         });
@@ -326,10 +331,10 @@ var index = (function (firebase) {
     },
 
     getManyReference: function getManyReference(source, params) {
-      var resource = [settings.context, source].join('/');
+      var resource = [settings.context, source].join("/");
       var getManyRef = new Promise(function (resolve, reject) {
         var data = params.ids.map(function (id) {
-          return database.ref([resource, id].join('/')).once('value').then(function (snapshot) {
+          return database.ref([resource, id].join("/")).once("value").then(function (snapshot) {
             return snapshot.val();
           });
         });
@@ -347,12 +352,12 @@ var index = (function (firebase) {
           while (1) {
             switch (_context3.prev = _context3.next) {
               case 0:
-                resource = [settings.context, source].join('/');
+                resource = [settings.context, source].join("/");
                 updateMany = new Promise(function (resolve, reject) {
                   params.ids.map(function (id) {
-                    database.ref([resource, id].join('/')).once('value').then(function (snapshot) {
+                    database.ref([resource, id].join("/")).once("value").then(function (snapshot) {
                       var update = new Promise(function (resolve, reject) {
-                        database.ref([resource, id].join('/')).set(Object.assign(snapshot.val(), params.data));
+                        database.ref([resource, id].join("/")).set(Object.assign(snapshot.val(), params.data));
                         resolve();
                       });
                       return update.then(function (res) {
@@ -362,12 +367,12 @@ var index = (function (firebase) {
                   });
                   resolve();
                 });
-                return _context3.abrupt('return', updateMany.then(function (res) {
+                return _context3.abrupt("return", updateMany.then(function (res) {
                   return { data: params.ids };
                 }));
 
               case 3:
-              case 'end':
+              case "end":
                 return _context3.stop();
             }
           }
@@ -380,9 +385,9 @@ var index = (function (firebase) {
     }(),
 
     delete: function _delete(source, params) {
-      var resource = [settings.context, source].join('/');
+      var resource = [settings.context, source].join("/");
       var Delete = new Promise(function (resolve, reject) {
-        database.ref([resource, params.id].join('/')).set(null);
+        database.ref([resource, params.id].join("/")).set(null);
         resolve();
       });
       return Delete.then(function (res) {
@@ -391,10 +396,10 @@ var index = (function (firebase) {
     },
 
     deleteMany: function deleteMany(source, params) {
-      var resource = [settings.context, source].join('/');
+      var resource = [settings.context, source].join("/");
       var deleteMany = new Promise(function (resolve, reject) {
         params.ids.map(function (id) {
-          return database.ref([resource, id].join('/')).set(null);
+          return database.ref([resource, id].join("/")).set(null);
         });
         resolve();
       });
